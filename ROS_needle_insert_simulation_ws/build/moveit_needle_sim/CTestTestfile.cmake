b87b989e94f9aec0d15f6ed4a708aa431b224566
Dang The Hung <dthung.y17@gmail.com>
2024-05-30 16:00:30 +0100

    remove some unecessary file

--- ROS_needle_insert_simulation_ws/build/moveit_needle_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rosbox/needle_insert_simulation_ws/src/moveit_needle_sim
-# Build directory: /home/rosbox/needle_insert_simulation_ws/build/moveit_needle_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ROS_needle_insert_simulation_ws/build/needle_path_simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rosbox/needle_insert_simulation_ws/src/needle_path_simulation
-# Build directory: /home/rosbox/needle_insert_simulation_ws/build/needle_path_simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
